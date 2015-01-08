df26de23525d81c50bc54cc8b1e4a5d571b570c2
cPanelSamir <samir.jafferali@cpanel.net>
2015-01-07 22:48:39 -0600

    Fixed gramatical errors in disclaimer and bug report

--- csi.pl
+++ csi.pl
@@ -651,18 +651,18 @@ sub bincheck {
         foreach (@debuglist) {
             print MAIL " ".$_."\n";
         }
-        print MAIL " -DEBUG STOP-\n";
+        print MAIL "  -DEBUG STOP-\n";
         print MAIL " --REPORT END--\n";
         close(MAIL);
-        print "Bug report sent. If for some reason, outbound email is not functional on this server, please copy and paste the below output to the script maintainer.\n\n";
+        print "Bug report sent. If outbound email is not functional on this server, please send the below output to the script maintainer.\n\n";
         print " --REPORT START--\n";
         print $message ; 
         print " -DEBUG START-\n";
         foreach (@debuglist) {
             print " ".$_."\n";
         }
-        print " -DEBUG STOP-\n";
-        print " --REPORT END--\n";
+        print "  -DEBUG STOP-\n";
+        print " --REPORT END--\n\n\n";
     }
 }
 
@@ -670,12 +670,12 @@ sub disclaimer {
     print_normal('');
     print_header('########################################################################');
     print_header('### DISCLAIMER! cPanel\'s Technical Support does not provide            #');
-    print_header('### security consultations services. The only support services we      #');
+    print_header('### security consultation services. The only support services we      #');
     print_header('### can provide at this time is to perform a minimal analysis of the   #');
     print_header('### possible security breach solely for the purpose of determining if  #');
     print_header('### cPanel\'s software was involved or used in the security breach.     #');
     print_header('########################################################################');
-    print_header('### If it is suspect to be root compromised, only Level III Analysts   #');
+    print_header('### If it is suspected to be root compromised, only Level III Analysts   #');
     print_header('### should be handling the issue. Account level compromises are        #');
     print_header('### investigated as a courtesy and carry no guarantees.                #');
     print_header('########################################################################');
